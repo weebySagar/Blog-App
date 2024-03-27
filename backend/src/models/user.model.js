849f45f7ea2a371a592e0269d0ca8f1e874f7a2e
Sagar Moriya <sagarmaurya814@gmail.com>
2024-03-27 13:08:22 +0530

    added user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+
+export default mongoose.model('User', User);
